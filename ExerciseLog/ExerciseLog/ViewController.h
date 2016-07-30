9af903451def145719e4daef04900f734ebdd52c
seaschi <seaschi@seans-mac-pro.guest.corp.microsoft.com>
2016-07-29 19:19:56 -0700

    initial add

---
+++ ExerciseLog/ExerciseLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExerciseLog
+//
+//  Created by seaschi on 7/29/16.
+//  Copyright © 2016 seaschi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
